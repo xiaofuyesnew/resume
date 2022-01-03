3703ee8f724f7581c793991b76f65e831d0eb387
xiaofuyesnew <xiaofuyesnew@qq.com>
2022-01-03 21:20:22 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ const App = () => {
 
   return (
     <div className="w-screen h-screen overflow-auto flex flex-col items-center p-[12px] relative">
-      <div className="w-[800px] min-h-[100%]">
+      <div className="w-[800px] min-h-[100%] shadow-md">
         简历主体
+        <div className="before:content-['这是一个伪类'] before:text-[20px]"></div>
       </div>
       <div className="fixed left-[calc(50%+420px)] top-0 w-[140px] h-screen p-[12px]">
         简历操作
