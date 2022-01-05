5cd7e253d222f224f167acb600718f41c0d8a01d
xiaofuyesnew <xiaofuyesnew@qq.com>
2022-01-05 16:35:20 +0800

    update

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('./dist/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
